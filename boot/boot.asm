564f97bc60893729177f79c4a750f5b3783db68e
Johnson Lee <g.johnsonlee@gmail.com>
2013-07-07 03:38:00 +0800

    Add util library

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,27 +4,22 @@
     org     0x7C00          ; boot from 0000:7C00
 %endif
 
+    call    scrnloc
+
     mov     ax, cs
     mov     ds, ax
     mov     es, ax
+    mov     ax, boot_msg
+    mov     cx, boot_msg_len
     call    prints
 
     jmp     $
 
-prints:
-    mov     ax, boot_msg
-    mov     bp, ax          ; es:bp -> offset of string
-    mov     cx, 13          ; string length
-    mov     ah, 0x13        ; write string to the screen
-    mov     al, 0x01        ; write mode (string is chars only, attribute in BL, cursor moved)
-    mov     bh, 0x00        ; the page number
-    mov     bl, 0x0F        ; the color of text
-    mov     dh, 0x00        ; row number
-    mov     dl, 0x00        ; column number
-    int     0x10            ; BIOS Video Display Services
-    ret
+%include "../lib/print.inc"
+%include "../lib/screen.inc"
 
 boot_msg:               db  "Hello, Koala!"
+boot_msg_len:           equ $-boot_msg
 
 times       510-($-$$)  db  0
 
